ec9cf6814802ac3294c5576d57d471b4c232a4bb
anna <anna@sayf.su>
2023-08-03 14:25:53 +0300

    initial commit

---
+++ src/main/java/com/example/course_easyauction/CourseEasyAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.example.course_easyauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseEasyAuctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseEasyAuctionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/course_easyauction/CourseEasyAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.course_easyauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseEasyAuctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
